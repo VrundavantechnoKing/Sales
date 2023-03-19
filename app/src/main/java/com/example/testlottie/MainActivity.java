c5dd18f9db3cb58635b8ebaa6dbf64a55593f953
Abhijeet Deshmukh <abhijeetdeshmukh.2765@gmail.com>
2023-03-19 09:08:26 +0530

    git commit -m "first commit"

---
+++ app/src/main/java/com/example/testlottie/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.testlottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
