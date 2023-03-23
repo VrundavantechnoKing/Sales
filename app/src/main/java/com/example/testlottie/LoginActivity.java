9d0c30f43bf6c482d8517b79dd3cc800c2623772
Abhijeet Deshmukh <abhijeetdeshmukh.2765@gmail.com>
2023-03-23 15:05:08 +0530

    git commit -m "Completed Login form structure design as par figma tamplet with leftout work Day-4"

--- app/src/main/java/com/example/testlottie/LoginActivity.java
+++ app/src/main/java/com/example/testlottie/LoginActivity.java
@@ -2,13 +2,25 @@ package com.example.testlottie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private TextView SignUp_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SignUp_btn=findViewById(R.id.Signup_btn);
+
+        SignUp_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+            }
+        });
     }
 }
