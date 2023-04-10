8ab377f3004ab09801e522ba8d7362fd59fd453e
Abhijeet Deshmukh <abhijeetdeshmukh.2765@gmail.com>
2023-04-10 13:44:12 +0530

    git commit -m "Completed the design of LoginActivity and RegistrationActivity"

--- app/src/main/java/com/example/testlottie/RegistrationActivity.java
+++ app/src/main/java/com/example/testlottie/RegistrationActivity.java
@@ -2,13 +2,25 @@ package com.example.testlottie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    private TextView SignIn_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        SignIn_btn=findViewById(R.id.Signin_btn);
+
+        SignIn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
